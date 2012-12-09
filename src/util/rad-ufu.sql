b29058b6a229097ad2ee2298183cde4a0384d5cd
pablohenrique <pablo.haoko@gmail.com>
2012-12-09 17:02:12 -0200

    modificacoes no sql

--- src/util/rad-ufu.sql
+++ src/util/rad-ufu.sql
@@ -1,15 +1,14 @@
 CREATE DATABASE RADUFU;
 
 CREATE TABLE categoria(
-	categoria_id 	serial PRIMARY KEY,
-	categoria_nome 	varchar(30)
+	categoria_nome 	varchar(30) PRIMARY KEY
 );
 
 CREATE TABLE multiplicador(
-	multiplicador_id 		serial PRIMARY KEY,
-	multiplicador_nome 		varchar (30),
-	multiplicador_valor 	smallint,
-	multiplicador_limite	smallint
+	multiplicador_id 				serial PRIMARY KEY,
+	multiplicador_nome 				varchar (30),
+	multiplicador_valor 			smallint,
+	multiplicador_limite			smallint
 	multiplicador_tipo_atividade    int,
 	FOREIGN KEY (multiplicador_tipo_atividade) REFERENCES tipo(tipo_codigo)
 	ON UPDATE CASCADE ON DELETE CASCADE
@@ -22,35 +21,33 @@ CREATE TABLE tipo(
 	tipo_pontuacao 				smallint,
 	tipo_pontuacaoreferencia 	smallint,
 	tipo_pontuacaolimite 		smallint,
-	FOREIGN KEY (tipo_categoria) REFERENCES categoria(categoria_id)
+	FOREIGN KEY (tipo_categoria) REFERENCES categoria(categoria_nome)
 	ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE atividade(
 	atividade_id			serial PRIMARY KEY,
-	atividade_tipo			int,
-	atividade_descricao 	varchar(255),
+	atividade_tipo			int NOT NULL,
+	atividade_descricao 	varchar(255) NOT NULL,
 	atividade_datainicio 	date,
 	atividade_datafim 		date,
-	atividade_professor		integer,
+	atividade_professor		integer NOT NULL,
 	FOREIGN KEY (atividade_tipo) REFERENCES tipo(tipo_codigo),
-	FOREIGN KEY (atividade_professor) REFERENCES tipo(professor_id)
-	ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY (atividade_professor) REFERENCES tipo(professor_siape),
 );
 
 CREATE TABLE comprovante(
-	comprovante_id 			serial PRIMARY KEY,
-	comprovante_arquivo 	varchar(300),
+	comprovante_arquivo 	varchar(300) PRIMARY KEY,
 	comprovante_atividade 	integer,
 	FOREIGN KEY (comprovante_atividade) REFERENCES atividade(atividade_id)
-	ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE professor(
-	professor_id 		serial PRIMARY KEY,
-	professor_nome 		varchar(30),
-	professor_sobrenome	varchar(30),
-	professor_usuario	varchar(15),
-	professor_senha		varchar(40),
-	professor_siape		integer
+	professor_siape		integer PRIMARY KEY,
+	professor_nome 		varchar(30) NOT NULL,
+	professor_sobrenome	varchar(30) NOT NULL,
+	professor_usuario	varchar(15) NOT NULL,
+	professor_senha		varchar(40) NOT NULL,
+	UNIQUE(professor_usuario)
+	ON DELETE CASCADE
 );
