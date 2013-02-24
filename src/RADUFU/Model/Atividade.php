7b375625a02878a28b8b492b1f17c0fa41838f10
Pablo Henrique <pablohenrique@linuxmint.(none)>
2013-02-24 14:25:54 -0300

    modificacoes no Atividade model

--- src/RADUFU/Model/Atividade.php
+++ src/RADUFU/Model/Atividade.php
@@ -10,8 +10,9 @@ class Atividade implements JsonSerializable{
     private $descricao;
     private $datainicio;
     private $datafim;
-    private $valor;
-    private $professor;
+    private $valorMult;
+    private $comprovante = array();
+    //private $professor;
 
     /*GETTERS*/
     public function getId(){return $this->id;}
@@ -19,9 +20,9 @@ class Atividade implements JsonSerializable{
     public function getDescricao(){return $this->descricao;}
     public function getDataInicio(){return $this->datainicio;}
     public function getDataFim(){return $this->datafim;}
-    /*modificado o campo abaixo*/
-    public function getValor(){ return $this->valor;}
-    public function getProfessor(){return $this->professor;}
+    public function getValorMult(){ return $this->valorMult;}
+    //public function getProfessor(){return $this->professor;}
+    public function getComprovante(){ return $this->comprovante }
 
     /*SETTERS*/
     public function setId($input){$this->id = $input;}
@@ -29,9 +30,20 @@ class Atividade implements JsonSerializable{
     public function setDescricao($input){$this->descricao = $input;}
     public function setDataInicio($input){$this->datainicio = $input;}
     public function setDataFim($input){$this->datafim = $input;}
-    /*modificado o campo abaixo*/
-    public function setValor($input){$this->valor = $input;}
-    public function setProfessor($input){$this->professor = $input;}
+    public function setValorMult($input){$this->valorMult = $input;}
+    //public function setProfessor($input){$this->professor = $input;}
+    public function setComprovante(Comprovante $comprovante){ self::add($comprovante); }
+
+    public function add(Comprovante $comprovante){
+        if(!in_array($comprovante, $this->comprovante))
+            self::getComprovante()[] = $this->comprovante;
+    }
+
+    public function remove(Comprovante $comprovante){
+        $key = array_search($comprovante, $this->comprovante);
+        if($key != FALSE)
+            unset($this->comprovante[$key]);
+    }
 
     public function JsonSerialize() {
         return [
@@ -40,7 +52,9 @@ class Atividade implements JsonSerializable{
             'descricao' => $this->getDescricao(),
             'datainicio' => $this->getDataInicio(),
             'datafim' => $this->getDataFim(),
-            'professor' => $this->getProfessor()
+            'valorMult' => $this->getValorMult(),
+            /*'professor' => $this->getProfessor(),*/
+            'comprovante' => $this->getComprovante()
         ];
     }
   }
